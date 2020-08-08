2d287e40a6f0fc84479c62acbc617663d1adf0a9
ax94242 <56212960+ax94242@users.noreply.github.com>
2020-08-08 14:41:06 +0530

    V1
    Improved

---
+++ docs/doc2.md
@@ -0,0 +1,58 @@
+---
+id: doc2
+title: document number 2
+---
+
+This is a link to [another document.](doc3.md)  
+July 2020 Update
+United Kingdom
+
+In April, we notified you that the Association of the British Pharmaceutical Industry (ABPI) had released guidance that 2019 data would be submitted and disclosed in the aggregate in an effort to ease the burden on healthcare professionals to review the data during the initial stages of the COVID-19 pandemic.
+
+ABPI has published the 2019 aggregated data, but is now requesting companies "disaggregate" their data and disclose at the individual level. Reports with disaggregated 2019 transfers of value are to be submitted by 31 July 2020. Disaggregated data refers to the individual disclosures for recipients who provided consent for standard disclosure.
+
+The submission portal will be open for companies to submit from Monday 20 July to Friday 31 July at 14:00 GMT.
+
+ABPI also asked companies to confirm that the already published 2019 aggregate data is up to date. The aggregate data will remain published until the official disaggregated data is made available to the public.
+ 
+
+June 2020 Update
+Greece
+
+Recently the National Organization for Medicines (EOF) announced that the deadline for 2019 local EOF disclosure was moved up from September 30, 2020 to June 30, 2020. No change has been noted for the local submission process.
+
+Colombia
+
+On May 29, 2020, the Colombia Ministry of Health and Social Protection announced that companies will now be able to submit data that was missed from the report due in March 2020. The Registry of Transfers of Value of the Health Sector (“RTVSS”) will be open from June 1-30, 2020 to submit the late data. 
+
+May 2020 Update
+Netherlands
+
+In light of COVID-19, the Healthcare Transparency Register Foundation (Stichting Transparantieregister Zorg) has announced that the transparency reporting deadline for 2019 reporting has been extended from 1 June 2020 to 15 June 2020. No change has been noted for the submission process to the transparency register.
+
+April 2020 Update
+United Kingdom
+
+The Association of the British Pharmaceutical Industry (ABPI) hosted a webcast to discuss the latest guidance for 2019 transparency disclosures. Their decision has been made to only publish aggregate data on the Disclosure UK portal. Companies are advised to submit all healthcare professional (HCP) and healthcare organization (HCO) data in aggregate, along with research and development. The only exception is joint working arrangements, which will be provided in methodological notes. ABPI will provide an amended, temporary data template for companies to submit 2019 data.
+
+Here are next steps for your consideration:
+
+Complete a temporary, aggregated 2019 data template provided by ABPI
+Submit the aggregated 2019 data template to Disclosure UK portal in May or June 2020 (exact dates to be communicated by ABPI)
+Update your 2019 methodological notes to account for the latest guidance. ABPI will share a sample excerpt that provides details on the circumstances surrounding COVID-19 and how to include joint working payments
+Continue to respond and resolve inquiries raised for any historic data from 2016-2018 in the Disclosure UK portal (noting that 2016 data will be removed from the portal 30 June, 2020). For any data amendments, you can continue to submit as standard. However, ABPI may not publish until it is deemed appropriate
+Keep your original, disaggregated 2019 data template and original methodological notes, as submission to the Disclosure UK portal is expected later this year
+If your company decides to move forward to publish 2019 data on company website: 
+Do not contact HCPs/HCOs, unless required to contractually or by company standard operating procedure
+It is recommended to remove email addresses or third party IDs from the template disclosed
+Ireland
+
+Recently, the Irish Pharmaceutical Healthcare Association (IPHA) shared more information regarding consent trends and reporting in light of COVID-19. Guidance shared from IPHA differs from the direction ABPI has taken by asking companies to continue individual disclosure where appropriate consent has been captured. Therefore, no changes are asked to be made due to COVID-19 for 2019 data.
+
+Here are the key takeaways:
+
+Consent rates have risen consistently year over year
+Companies should continue to report recipients on the individual level, where appropriate consent has been captured, for 2019 data
+Encourage companies not to aggregate but to aim for high levels of individual level disclosure to maintain transparency in society
+There are no restrictions on sending reminder letters to HCPs
+Disclosure reports should include rounded transfer of value amounts (e.g., no decimals) for 2019 data and onwards
---
+++ website/pages/en/index.js
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = (props) => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Logo = (props) => (
+      <div className="projectLogo">
+        <img src={props.img_src} alt="Project Logo" />
+      </div>
+    );
+
+    const ProjectTitle = (props) => (
+      <h2 className="projectTitle">
+        {props.title}
+        <small>{props.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = (props) => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = (props) => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    return (
+      <SplashContainer>
+        
+        <div className="inner">
+          <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
+          <PromoSection>
+            
+          </PromoSection>
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const Block = (props) => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+    const Description = () => (
+      <Block background="dark">
+        {[
+          {
+          
+            title: 'Get the most up-to-date information regarding the evolving nature of spend transparency in the pharmaceutical and life sciences and health industries.',
+          },
+        ]}
+      </Block>
+    );
+
+    const FeatureCallout = () => (
+      <div
+        className="productShowcaseSection paddingBottom"
+        style={{textAlign: 'center'}}>
+        <h2>July 2020 Update</h2>
+        <h3>United Kingdom</h3>
+        <p>In April, we notified you that the Association of the British Pharmaceutical Industry (ABPI) had released guidance that 2019 data would be submitted and disclosed in the aggregate in an effort to ease the burden on healthcare professionals to review the data during the initial stages of the COVID-19 pandemic.</p>
+        <p>ABPI has published the 2019 aggregated data, but is now requesting companies "disaggregate" their data and disclose at the individual level. Reports with disaggregated 2019 transfers of value are to be submitted by 31 July 2020. Disaggregated data refers to the individual disclosures for recipients who provided consent for standard disclosure.</p>
+        <p>The submission portal will be open for companies to submit from Monday 20 July to Friday 31 July at 14:00 GMT.</p>
+        <p>ABPI also asked companies to confirm that the already published 2019 aggregate data is up to date. The aggregate data will remain published until the official disaggregated data is made available to the public.</p>
+      </div>
+    );
+    
+
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
+
+      const showcase = siteConfig.users
+        .filter((user) => user.pinned)
+        .map((user) => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
+
+      const pageUrl = (page) =>
+        baseUrl + (language ? `${language}/` : '') + page;
+
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using This?</h2>
+          <p>This project is used by all these people</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl('users.html')}>
+              More {siteConfig.title} Users
+            </a>
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <Description />
+          <FeatureCallout />
+          <Showcase />
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
